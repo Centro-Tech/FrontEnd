7643f7cbc21552401fc9d8c45fa19afc8f1061a4
Julia-Apolinario <julia.apolinario@sptech.school>
2025-11-18 20:15:26 -0300

    ajustes

--- Frontend-projetoMima/projeto-MimaStore/vite.config.js
+++ Frontend-projetoMima/projeto-MimaStore/vite.config.js
@@ -6,24 +6,3 @@ export default defineConfig({
   plugins: [react()],
 
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig(() => {
-  
-//   return {
-//     plugins: [react()],
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: 'http://localhost:8080',
-//           changeOrigin: true,
-//           secure: false,
-//           rewrite: (path) => path.replace(/^\/api/, '')
-//         }
-//       }
-//     }
-//   }
-// })
